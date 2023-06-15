1db4f3e2de0c500665c298ac95c5eff6e7536baf
Jonas <jonas@jole.xyz>
2023-06-15 15:06:07 +0200

    clean

--- src/download.rs
+++ src/download.rs
@@ -13,31 +13,30 @@ pub struct Download {
     // where to place challenges
     #[argh(positional)]
     out_path: PathBuf,
+
+    #[argh(switch, short = 'f')]
+    /// say yes
+    force: bool,
 }
 
 impl Download {
-    pub async fn run(&self, ctf: &Ctfd) -> Result<(), Report> {
-        let tasks = ctf.all_tasks().await?;
-
-        // make sure path exists
-        if !self.out_path.exists() {
-            println!(
-                "Diretory does not exist. Create '{}'? [Y/n]",
-                self.out_path.display()
-            );
-
-            // read y or n
-            let answer = read_line_lower();
-            if answer == "y" || answer.is_empty() {
-                std::fs::create_dir_all(&self.out_path).unwrap();
-            } else {
-                println!("Exiting...");
-                return Ok(());
-            }
+    fn make_path(&self) -> Result<(), Report> {
+        if self.out_path.exists() {
+            return Ok(());
         }
 
-        // make sure there are no files there yet
-        if self.out_path.read_dir().unwrap().count() > 0 {
+        std::fs::create_dir_all(&self.out_path)?;
+
+        Ok(())
+    }
+
+    fn root_write_approval(&self) -> Result<Option<()>, Report> {
+        let file_count = self.out_path.read_dir()?.count();
+        if file_count == 0 {
+            return Ok(Some(()));
+        }
+
+        let approval = self.force || {
             println!(
                 "Directory '{}' is not empty. Continue? [Y/n]",
                 self.out_path.display()
@@ -45,19 +44,52 @@ impl Download {
 
             // read y or n
             let answer = read_line_lower();
-            if answer != "y" && !answer.is_empty() {
-                println!("Exiting...");
-                return Ok(());
-            }
+            answer == "y" || answer.is_empty()
+        };
+
+        if approval {
+            Ok(Some(()))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn sanitize_name(name: &str) -> String {
+        name.replace(' ', "_")
+            .chars()
+            .into_iter()
+            .filter(|c| c.is_ascii_alphanumeric() || c == &'_')
+            .collect()
+    }
+
+    fn file_write_approval(&self, path: &PathBuf) -> Result<bool, Report> {
+        if !path.exists() {
+            // no file there, can safely write
+            return Ok(true);
         }
 
+        let approval = self.force || {
+            println!("File '{}' already exists. Overwrite? [y/N]", path.display());
+
+            let answer = read_line_lower();
+            answer == "y"
+        };
+
+        Ok(approval)
+    }
+
+    pub async fn run(&self, ctf: &Ctfd) -> Result<(), Report> {
+        self.make_path()?;
+
+        if self.root_write_approval()?.is_none() {
+            // early exit, no write approval
+            return Ok(());
+        }
+
+        let tasks = ctf.all_tasks().await?;
+
         for t in &tasks {
-            let folder = t.name.to_lowercase().replace(' ', "_");
-            let folder: String = folder
-                .chars()
-                .into_iter()
-                .filter(|c| c.is_ascii_alphanumeric() || c == &'_')
-                .collect();
+            let folder = Self::sanitize_name(&t.name);
 
             let path = self.out_path.join(folder);
             println!(
@@ -67,34 +99,23 @@ impl Download {
             );
 
             if !path.exists() {
-                std::fs::create_dir(path.clone()).unwrap();
+                std::fs::create_dir(path.clone())?;
             }
 
             for d in &t.downloads {
-                let url: Url = format!("{base}{path}", base = ctf.base_url(), path = d)
-                    .parse()
-                    .unwrap();
+                let url: Url = format!("{base}{path}", base = ctf.base_url(), path = d).parse()?;
 
                 // get file name
                 let file_name = url.path_segments().unwrap().last().unwrap();
                 let file_path = path.join(file_name);
-                //println!("Downloading to {}", file_path.display());
 
-                if file_path.exists() {
-                    println!(
-                        "File '{}' already exists. Overwrite? [y/N]",
-                        file_path.display()
-                    );
+                let can_write = self.file_write_approval(&file_path)?;
 
-                    let answer = read_line_lower();
-                    if answer == "y" {
-                        std::fs::create_dir_all(&self.out_path).unwrap();
-                    } else {
-                        // default: no
-                        println!("Skipping");
-                        continue;
-                    }
+                if !can_write {
+                    println!("Skipping...");
+                    continue;
                 }
+
                 let mut file = std::fs::File::create(file_path).expect("couldn't create file");
 
                 let resp = ctf.client().get(url).send().await?;
